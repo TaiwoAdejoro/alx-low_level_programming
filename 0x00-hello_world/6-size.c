3c4daed8f87e8bcfd2c45a114c16bb828b72bd40
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-08 20:00:19 -0700

    Tasks 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-*main - print "program that prints the size of various types on the computer"
+* main - print "program that prints the size of various types on the computer"
 *
-*Return: 0 if exited properly, non-zero otherwise
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
