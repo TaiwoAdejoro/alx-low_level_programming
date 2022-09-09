ed9956dbe20e699496505c9436de24df2ea47fef
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-08 19:49:58 -0700

    Tasks 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - print "program that prints the size of various types on the computer"
+*
+*Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u bytes(s)\n" sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n" sizeof(float));
+	return (0);
+}
