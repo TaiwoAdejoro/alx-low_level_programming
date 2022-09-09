78af6915aa64cd967ed055df01805a7497d4967b
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-08 17:40:15 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
-int main() {
-
-	  puts("\"Programming is like building a multilingual puzzle");
-
-	    return 0;
-
+/**
+* main - Print "Programming is like building a multilingual puzzle"
+* Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 /**
- *
- *   * main - Entry point
- *
- *   *
- *
- *   * Return: Always 0 (Success)
- *
- *     */
-
+* main - print 'with proper grammar, but the outcome is a piece of art'
+*Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-   printf("with proper grammar, but the outcome is a piece of art, \n")
-	    return (0);
-
+printf("with proper grammar, but the outcome is a piece of art.\n");
+return (0);
 }
