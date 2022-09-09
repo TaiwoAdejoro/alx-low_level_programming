c41fa509ae8fe01b46c1cb5356884c50430d1e1b
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-08 21:11:06 -0700

    Tasks 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned int) sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned int) sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned int) sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned int)sizeof(long long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned int) sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned int) sizeof(float));
 	return (0);
 }
