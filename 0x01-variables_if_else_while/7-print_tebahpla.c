7a18895f2603bb535e6d2237927ea02746a239f7
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-09 18:24:29 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+int main(void)
+{
+char (alphabet = 122; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
