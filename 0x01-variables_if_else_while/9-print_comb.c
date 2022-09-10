d9263b58f7ff771fd26d772cc98af942de6874a8
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-09 19:10:48 -0700

    9-print_com

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int a;
+for (a = 48; number <= 57; number++)
+{
+putchar(a);
+if (a == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+putchar ('\n')
+return (0);
+}
