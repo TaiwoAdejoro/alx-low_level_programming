39af3646f7a67a744202481ea0bf1b71a01c3c00
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-13 14:51:35 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+* main - This program prints _putchar, followed by a new lineprogram.
+*
+* Return - Always 0.
+*/
+int main(void)
+{
+char church[5] = "_putchar"
+int i;
+for (i = 0; i < 5; i++)
+{
+_putchar(church[i]);
+}
+_putchar(5);
+return (0);
+}
