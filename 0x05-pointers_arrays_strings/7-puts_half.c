6384d443d0d1ba6d576d5c0fa58856a77399c33e
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-19 13:43:36 -0700

    Tasks 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -25,9 +25,9 @@ void puts_half(char *str)
 		}
 	} else if (len % 2)
 	{
-		for (n = (len - 1) / 2; n < len - 1; x++)
+		for (x = (len - 1) / 2; x < len - 1; x++)
 		{
-			_putchar(str[n + 1]);
+			_putchar(str[x + 1]);
 		}
 	}
 	_putchar('\n');
