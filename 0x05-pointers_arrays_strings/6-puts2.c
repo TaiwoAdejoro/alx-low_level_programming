c9edf6b0c8bdb513db1ee674297c5e47faadd31e
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-19 13:24:22 -0700

    Tasks 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int len, j;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (j = 0; j < len; j += 2)
+	{
+		_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
