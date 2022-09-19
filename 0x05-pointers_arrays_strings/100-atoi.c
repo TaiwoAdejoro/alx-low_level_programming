bbae643b479143167cb084a0ba06353d00297b18
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-19 14:21:31 -0700

    Tasks 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int a, b, c, len, d, digit;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	len = 0;
+	d = 0;
+	num = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (a < len && b == 0)
+	{
+		if (s[a] == '-')
+			++b;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			digit = s[a] - '0';
+			if (b % 2)
+				digit = -digit;
+			c = c * 10 + digit;
+			d = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			f = d;
+		}
+		a++;
+	}
+
+	if (d == 0)
+		return (0);
+
+	return (n);
+}
