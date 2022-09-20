a47a8c129ae8fe0fb53d283c11345940bcd6a9c8
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-20 10:40:05 -0700

    Tasks 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -30,7 +30,7 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 + num;
+			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
