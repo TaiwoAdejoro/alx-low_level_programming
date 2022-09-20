e9e7111f5e2396080fad74c50d93f6aa76820105
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-20 10:53:09 -0700

    Tasks 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pass[100];
+	int j, sum, k;
+
+	sum = 0;	
+
+	srand(time(NULL));
+
+	for (j = 0; j < 100; j++)
+	{
+		pass[j] = rand() % 78;
+		sum += (pass[j] + '0');
+		putchar(pass[j] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			k = 2772 - sum - '0';
+			sum += k;
+			putchar(k + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
