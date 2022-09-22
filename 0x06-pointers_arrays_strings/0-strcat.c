a066e1b90c2395f84ee7df4b44dea1d8c8b89b99
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-22 07:02:32 -0700

    Tasks 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int x, y;
+	int i, j;
 
-	x = 0;
-	y = 0;
+	i = 0;
+	j = 0;
 
-	while (dest[x] != '\0')
+	while (dest[i] != '\0')
 		x++;
 
-	while (src[y] != '\0')
+	while (src[j] != '\0')
 	{
-		dest[x] = src[j];
-		y++;
-		x++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
 
-	dest[x] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
