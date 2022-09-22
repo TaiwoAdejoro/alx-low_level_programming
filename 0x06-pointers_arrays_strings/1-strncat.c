f75f363cf6cd25640ee2ceb99dd29c7969721119
TaiwoAdejoro <taiwoadejoro@gmai.com>
2022-09-22 07:21:04 -0700

    Tasks 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int k;
+
+	k = 0;
+
+	while (src[k] != '\0' && k < n)
+	{
+		dest[k] = src[k];
+		k++;
+	}
+
+	while (k < n)
+	{
+		dest[k] = '\0';
+		k++;
+	}
+
+	return (dest);
+}
