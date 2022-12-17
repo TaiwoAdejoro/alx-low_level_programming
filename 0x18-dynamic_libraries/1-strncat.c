db3341652ee7fb6d3fd195e8d1d6b87bc5d4056c
Taiwo Adejoro <taiwoadejoro@gmai.com>
2022-12-16 17:16:31 -0800

    Tasks 0

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, k;
+
+	i = 0;
+	k = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[k] != '\0' && k < n)
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+
+}
+
